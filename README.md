a8683897d2282da6d2be5dfa1448a878975b644f
zhouhuajian <zhouhuajian@company.com>
2023-03-27 17:26:16 +0800

    多线程 I/O 多路复用

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 粘包、半包、消息边界、大数据无法一次write传输、大数据无法一次read接收
 
+动态分配缓冲区
+
++ 方案1 大小不够动态扩容*2 优点简单方便 缺点扩容和复制 性能较差
++ 方案2 缓冲区列表，4K缓冲区，超出新的4K缓冲区，加入到列表 优点不需要复制数据，缺点处理时需要再拼接
++ 方案3 自定义协议，协议头明确数据大小，准确分配具体缓冲区大小，优点 空间资源利用率最高， 缺点自定义协议，实现可能较复杂
+
 ### 单线程 阻塞IO Blocking I/O
 
 accept 阻塞的时候，不能 read  
@@ -15,7 +21,7 @@ accept read 非阻塞
 
 可正常 accept read 但 CPU 长时间空转 电脑风扇会响 约使用 20% CPU
 
-### 单线程 多路复用 I/O multiplexing
+### 单线程 I/O 多路复用 multiplexing
 
 或叫做事件驱动IO
 
@@ -29,6 +35,29 @@ public static final int OP_WRITE = 1 << 2;
 public static final int OP_CONNECT = 1 << 3;
 // 有客户端连接时，触发OP_ACCEPT事件
 public static final int OP_ACCEPT = 1 << 4;
+```
+
+### 多线程 I/O 多路复用 
+
+```text
+Connected to the target VM, address: '127.0.0.1:54152', transport: 'socket'
+17:22:38.068 [main] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- boss selecting...
+17:22:38.068 [worker-1] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:22:38.068 [worker-0] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:22:43.560 [main] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- client connected java.nio.channels.SocketChannel[connected local=/127.0.0.1:8080 remote=/127.0.0.1:54166]
+17:22:43.563 [main] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- boss selecting...
+17:22:43.563 [worker-0] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:22:54.285 [main] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- client connected java.nio.channels.SocketChannel[connected local=/127.0.0.1:8080 remote=/127.0.0.1:54170]
+17:22:54.286 [main] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- boss selecting...
+17:22:54.286 [worker-1] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:23:20.574 [worker-0] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- data read abc
+17:23:20.574 [worker-0] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:23:26.975 [worker-0] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- data read abc
+17:23:26.976 [worker-0] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:23:42.652 [worker-1] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- data read 123
+17:23:42.653 [worker-1] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
+17:23:43.271 [worker-1] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- data read 123
+17:23:43.272 [worker-1] DEBUG org.example.tcp.MultiThreadIOMultiplexingServer -- worker selecting...
 ```
 
 ### 信号驱动 signal-driven I/O
---
+++ src/main/java/org/example/tcp/MultiThreadIOMultiplexingServer.java
@@ -0,0 +1,111 @@
+package org.example.tcp;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class MultiThreadIOMultiplexingServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress(8080));
+        // 必须设置为非阻塞 不然 IllegalBlockingModeException
+        ssc.configureBlocking(false);
+        Selector bossSelector = Selector.open();
+        ssc.register(bossSelector, SelectionKey.OP_ACCEPT);
+        AtomicInteger index = new AtomicInteger();
+        Worker[] workers = new Worker[]{new Worker(), new Worker()};
+        // blocking
+        while (true) {
+            log.debug("boss selecting...");
+            bossSelector.select();
+            Set selectedKeys = bossSelector.selectedKeys();
+            Iterator iter = selectedKeys.iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = (SelectionKey) iter.next();
+                iter.remove();
+                if (key.isAcceptable()) {
+                    SocketChannel sc = ssc.accept();
+                    log.debug("client connected {}", sc);
+                    Worker selectedWorker = workers[Math.abs(index.getAndIncrement() % 2)];
+                    selectedWorker.register(sc);
+                }
+            }
+        }
+    }
+
+    static class Worker implements Runnable {
+
+        static AtomicInteger count = new AtomicInteger();
+        Thread thread = null;
+        Selector selector = null;
+        ConcurrentLinkedQueue<SocketChannel> queue = null;
+
+        Worker() throws IOException {
+            thread = new Thread(this, "worker-" + count.getAndIncrement());
+            selector = Selector.open();
+            queue = new ConcurrentLinkedQueue<>();
+            thread.start();
+        }
+
+        public void register(SocketChannel sc) {
+            queue.add(sc);
+            selector.wakeup();
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    log.debug("worker selecting...");
+                    selector.select();
+                    SocketChannel channel = queue.poll();
+                    if (channel != null) {
+                        // 必须设置为非阻塞 不然 IllegalBlockingModeException
+                        channel.configureBlocking(false);
+                        channel.register(selector, SelectionKey.OP_READ);
+                    }
+                    Set selectedKeys = selector.selectedKeys();
+                    Iterator iterator = selectedKeys.iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey key = (SelectionKey) iterator.next();
+                        iterator.remove();
+                        if (key.isReadable()) {
+                            SocketChannel sc = (SocketChannel) key.channel();
+                            ByteBuffer byteBuffer = ByteBuffer.allocate(16);
+                            try {
+                                int read = sc.read(byteBuffer);
+                                if (read > 0) {
+                                    byteBuffer.flip();
+                                    log.debug("data read {}", Charset.defaultCharset().decode(byteBuffer).toString());
+                                } else if (read == -1) {
+                                    log.debug("client disconnected {}", sc);
+                                    key.cancel();
+                                }
+                            } catch (IOException e) {
+                                // 远程主机强迫关闭了一个现有的连接
+                                log.debug("client disconnected force {}", sc);
+                                key.cancel();
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
